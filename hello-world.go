6f805de0bf4c0906704842b6c819b0f713aa3577
subodh <subudear@yahoo.com>
2021-07-18 16:51:43 +1200

    new files

--- hello-world.go
+++ hello-world.go
@@ -8,6 +8,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello World</h1>")
 }
 
+
 func health(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Health Check Pass</h1>")
 }
